5695db40214ac1ccc4b9e8aeaf45c551e033f44b
gentlemanghost2 <davidudedeh@gmail.com>
2025-08-09 21:50:55 +0100

    made some grammatical corrections

--- madlibs.py
+++ madlibs.py
@@ -9,6 +9,6 @@ verb1 = input ('Enter a verb ending with (ing)')
 adjective3 = input('Enter an adjective(description):')
 
 print(f'Today I went to a {adjective1} zoo.')
-print(f'In an exhibit, I saw a {noun1}')
-print(f'{noun1} was {adjective2} and {verb1}')
+print(f'In an exhibit, I saw a {noun1}.')
+print(f'{noun1} was {adjective2} and {verb1}.')
 print(f'I was {adjective3}!')
