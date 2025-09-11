e32e42c2dbf507a72156f35799e291e4151479d1
gentlemanghost2 <davidudedeh@gmail>
2025-09-11 12:46:45 +0100

    practice for modules
    some function init

---
+++ example.py
@@ -0,0 +1,13 @@
+pi = 3.14159
+
+def squre(x):
+    return x ** 2 
+
+def cube(x):
+    return x ** 3 
+
+def circumference(radius):
+    2 * pi * radius
+    
+def area(radius):
+    return pi *  radius ** 2
